6490b2d1eeab83542967028cc2220fbc94271958
pk <pk@codeblob.work>
2026-08-11 11:15:57 +0200

    feat: jump between user messages

--- internal/ui/model/ui.go
+++ internal/ui/model/ui.go
@@ -2722,6 +2722,16 @@ func (m *UI) handleKeyPressMsg(msg tea.KeyPressMsg) tea.Cmd {
 				if cmd := m.chat.ScrollToSelectedAndAnimate(); cmd != nil {
 					cmds = append(cmds, cmd)
 				}
+			case key.Matches(msg, m.keyMap.Chat.UpUserItem):
+				m.chat.SelectPrevUser()
+				if cmd := m.chat.ScrollToSelectedAndAnimate(); cmd != nil {
+					cmds = append(cmds, cmd)
+				}
+			case key.Matches(msg, m.keyMap.Chat.DownUserItem):
+				m.chat.SelectNextUser()
+				if cmd := m.chat.ScrollToSelectedAndAnimate(); cmd != nil {
+					cmds = append(cmds, cmd)
+				}
 			case key.Matches(msg, m.keyMap.Chat.HalfPageUp):
 				if cmd := m.chat.ScrollByAndAnimate(-m.chat.Height() / 2); cmd != nil {
 					cmds = append(cmds, cmd)
